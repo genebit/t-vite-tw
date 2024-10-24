5c7acb544d332f63c3c6e555a6fa0464af1d1e27
Johcel Gene T. Bitara <jbitara@gbox.adnu.edu.ph>
2024-10-25 00:22:59 +0800

    chore: initial setup.

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <div className="container p-10 mx-auto">
+        <h1 className="text-4xl font-bold">
+          Hello, Vite + ReactTS + TailwindCSS + ShadcnUI!
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam
+          exercitationem tempora id nulla mollitia debitis tenetur unde, ad quas
+          minus!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
